b1b1fcff1215ebf6c0ebee0bb2aaac5147fb6322
tqios <tqiosn611@gmail.com>
2021-03-12 01:49:16 +0900

    removed hello.java and added manumanager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+	
+	public static void main(String[] args) {
+		System.out.println("1. Add Spending");
+		System.out.println("2. Delete Spending");
+		System.out.println("3. Edit Spending");
+		System.out.println("4. View Spending");
+		System.out.println("5. Exit");
+		
+	}
+
+}
