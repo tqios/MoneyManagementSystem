9f03552c1d67a0d6d9ded931f205958d74b7c548
tqios <tqiosn611@gmail.com>
2021-04-06 10:30:03 +0900

    add class

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,60 +3,28 @@ import java.util.Scanner;
 public class MenuManager {
 	
 	public static void main(String[] args) {
-		int num = 0;
 		Scanner input = new Scanner(System.in);
-		while (num != 6) {
+		SpendingManager spendingManager = new SpendingManager(input);
+		int num = 0;
+		while (num != 5) {
 			System.out.println("1. Add Spending");
 			System.out.println("2. Delete Spending");
 			System.out.println("3. Edit Spending");
 			System.out.println("4. View Spending");
-			System.out.println("5. Show a Menu");
-			System.out.println("6. Exit");	
-			System.out.print("Select one number between 1-6: ");
+			System.out.println("5. Exit");	
+			System.out.print("Select one number between 1-5: ");
 			num = input.nextInt();
 
 			if (num == 1) {
-				addSpending();
+				spendingManager.addSpending();
 			}if (num == 2) {
-				deleteSpending();
+				spendingManager.deleteSpending();
 			}if (num == 3) {
-				editSpending();
+				spendingManager.editSpending();
 			}if (num == 4) {
-				viewSpending();
+				spendingManager.viewSpending();
 			}else {
 				continue;}
 		}
-	}
-	
-	public static void addSpending() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("date+time(YYMMddhhmm) : ");
-		int date = input.nextInt();
-		System.out.print("money: ");
-		int money = input.nextInt();
-		System.out.print("Payment method : ");
-		String PaymentMethod = input.next();
-		System.out.print("Location : ");
-		String Location = input.next();
-		System.out.print("Memo : ");
-		String Memo = input.next();
-	}
-	
-	public static void deleteSpending() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("date+time(YYMMddhhmm) : ");
-		int date = input.nextInt();	
-	}
-	
-	public static void editSpending() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("date+time(YYMMddhhmm) : ");
-		int date = input.nextInt();	
-	}
-	
-	public static void viewSpending() {
-		Scanner input = new Scanner(System.in);
-		System.out.print("date+time(YYMMddhhmm) : ");
-		int date = input.nextInt();	
 	}
 }
